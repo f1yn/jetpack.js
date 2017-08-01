8f25165a95e571fb266a076635a4dcdbe24667f7
flynnham <dev@flynnbuckingham.com>
2017-08-01 17:52:11 -0400

    Added build sequence for flynn modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,40 @@
 
 var del = require('del');
+var fs = require('fs');
 var path = require('path');
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var fmModule = require('gulp-flynn-modulizer');
 
 const paths = {
     src: path.join(__dirname, './src'),
     build: path.join(__dirname, './dest')
 }
 
+const sourcePath = path.join(paths.src, './jetpack.js');
+
 gulp.task('clean', () => {
     return del(paths.build);
 });
 
-gulp.task('build', ['clean'], () => {
-    var source = gulp.src('./src/jetpack.js');
+gulp.task('build-fm', ['clean'], ()=>{
+   return fmModule.wrap({
+       name: 'jetpack',
+       constructorPath: sourcePath,
+       outputPath: path.join(paths.build, './jetpack-fm.js')
+   });
+});
+
+gulp.task('build', ['clean', 'build-fm'], () => {
+    var source = gulp.src(sourcePath);
 
     // normal (non-minified)
-    source
-        .pipe(gulp.dest(paths.build));
+    source.pipe(gulp.dest(paths.build));
 
     // normal minified
-    source
-        .pipe(uglify())
+    return source.pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest(paths.build));
 });
