c5f187a2a866b42ec79abd56e88c21115fcecc87
Flynn Buckingham <dev@flynnbuckingham.com>
2018-08-05 20:10:19 -0400

    Updated dist

--- dist/jetpack.js
+++ dist/jetpack.js
@@ -16,163 +16,168 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 /*__START_WRAP__*/
 var jetpack = function jetpack() {
-    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+	var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
 
-    var presetDurations = {
-        slow: 2000,
-        medium: 900,
-        fast: 400
-    };
+	var presetDurations = {
+		slow: 2000,
+		medium: 900,
+		fast: 400
+	};
 
-    var config = {
-        duration: presetDurations.medium,
-        updateURL: false,
-        animation: true
-    };
+	var config = {
+		duration: presetDurations.medium,
+		updateURL: false,
+		animation: true
+	};
 
-    var flags = {
-        hasListener: false
-    };
+	var flags = {
+		hasListener: false
+	};
 
-    var set = Object.freeze({
-        duration: function duration(value) {
-            switch (typeof value === "undefined" ? "undefined" : _typeof(value)) {
-                case "number":
-                    {
-                        config.duration = Math.abs(value);
-                    }
-                case "string":
-                    {
-                        var parsed = presetDurations[value] || value;
-                        if (!isNaN(parsed)) {
-                            config.duration = Number(parsed);
-                        }
-                    }
-                default:
-                    {
-                        // do nothing
-                    }
-            }
-        },
+	var set = Object.freeze({
+		duration: function duration(value) {
+			switch (typeof value === "undefined" ? "undefined" : _typeof(value)) {
+				case "number":
+					{
+						config.duration = Math.abs(value);
+					}
+				case "string":
+					{
+						var parsed = presetDurations[value] || value;
+						if (!isNaN(parsed)) {
+							config.duration = Number(parsed);
+						}
+					}
+				default:
+					{
+						// do nothing
+					}
+			}
+		},
 
-        updateURL: function updateURL(value) {
-            return config.updateURL = typeof value === "boolean" ? value : config.updateURL;
-        },
-        animation: function animation(value) {
-            return config.animation = typeof value === "boolean" ? value : config.animation;
-        }
-    });
+		updateURL: function updateURL(value) {
+			return config.updateURL = typeof value === "boolean" ? value : config.updateURL;
+		},
+		animation: function animation(value) {
+			return config.animation = typeof value === "boolean" ? value : config.animation;
+		}
+	});
 
-    // start setting config based on options
-    set.duration(options.duration);
-    set.updateURL(options.updateURL);
-    set.animation(options.animation);
+	// start setting config based on options
+	set.duration(options.duration);
+	set.updateURL(options.updateURL);
+	set.animation(options.animation);
 
-    /* START METHODS */
-    var _document = document,
-        documentElement = _document.documentElement,
-        body = _document.body;
-
-
-    var getScrollPosition = function getScrollPosition() {
-        return window.pageYOffset || documentElement.scrollTop;
-    };
-    var getScrollHeight = function getScrollHeight() {
-        return body.scrollHeight || documentElement.scrollHeight;
-    };
-
-    // transition functions for scrolls (currently only one)
-    var ease = {
-        InOutCubic: function InOutCubic(t, b, c, d) {
-            if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
-            return c / 2 * ((t -= 2) * t * t + 2) + b;
-        }
-    };
-
-    var scroll = function scroll() {
-        var delta = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-        var args = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-
-        args.callback = typeof args.callback === "function" ? args.callback : function () {};
-
-        // animation formula (more to be added later)
-        var startTime = null,
-            startPos = getScrollPosition(),
-            maxScroll = getScrollHeight() - window.innerHeight,
-            scrollEndValue = startPos + delta < maxScroll ? delta : maxScroll - startPos;
-
-        if (config.animation) {
-            var InOutCubic = ease.InOutCubic;
+	/* START METHODS */
+	var _document = document,
+	    documentElement = _document.documentElement,
+	    body = _document.body;
 
 
-            var scrollFrame = function scrollFrame(timestamp) {
-                startTime = startTime || timestamp;
-                var elapsed = timestamp - startTime;
+	var getScrollPosition = function getScrollPosition() {
+		return window.pageYOffset || documentElement.scrollTop;
+	};
+	var getScrollHeight = function getScrollHeight() {
+		return body.scrollHeight || documentElement.scrollHeight;
+	};
 
-                documentElement.scrollTop = body.scrollTop = InOutCubic(elapsed, startPos, scrollEndValue, config.duration);
-                elapsed < config.duration ? requestAnimationFrame(scrollFrame) : args.callback();
-            };
-            requestAnimationFrame(scrollFrame);
-        } else {
-            documentElement.scrollTop = body.scrollTop = scrollEndValue;
-        }
-    };
+	// transition functions for scrolls (currently only one)
+	var ease = {
+		InOutCubic: function InOutCubic(t, b, c, d) {
+			if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
+			return c / 2 * ((t -= 2) * t * t + 2) + b;
+		}
+	};
 
-    // scrolls to specific Y axis location in relation to the root scroll location
-    var scrollTop = function scrollTop() {
-        var pos = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-        var args = arguments[1];
-        return scroll(pos - getScrollPosition(), args);
-    };
+	var scroll = function scroll() {
+		var delta = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+		var args = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-    // scrolls to element on page (pass element as first argument, second argument optional)
-    var scrollToElement = function scrollToElement(elem, args) {
-        return (typeof elem === "undefined" ? "undefined" : _typeof(elem)) === "object" && elem !== null && scroll(elem.getBoundingClientRect().top, args);
-    };
+		args.callback = typeof args.callback === "function" ? args.callback : function () {};
 
-    var hookAnchors = function hookAnchors() {
-        if (!flags.hasListener) {
-            var listener = function listener(e) {
-                var target = e.target,
-                    hRef = target.getAttribute('href');
+		// animation formula (more to be added later)
+		var startTime = null,
+		    startPos = getScrollPosition(),
+		    maxScroll = getScrollHeight() - window.innerHeight,
+		    scrollEndValue = startPos + delta < maxScroll ? delta : maxScroll - startPos;
 
-                if (target.nodeName === 'A' && hRef.indexOf('#') < 2) {
-                    // fix for pages with trailing '/'
-                    hRef = hRef.indexOf('/#') === 0 ? hRef.substring(1) : hRef;
+		if (config.animation) {
+			var InOutCubic = ease.InOutCubic;
 
-                    if (hRef.charAt(0) === '#') e.preventDefault();
 
-                    if (hRef.length > 1) {
-                        if (elem = document.getElementById(hRef.substring(1))) {
-                            scrollToElement(elem, {
-                                callback: function callback() {
-                                    config.updateURL && (window.location.href = hRef);
-                                }
-                            });
-                        }
-                    } else {
-                        scrollToElement(body, {
-                            callback: function callback() {
-                                config.updateURL && (window.location.href = '#');
-                            }
-                        });
-                    }
-                }
-            };
+			var scrollFrame = function scrollFrame(timestamp) {
+				startTime = startTime || timestamp;
+				var elapsed = timestamp - startTime;
 
-            window.addEventListener('click', listener);
-            flags.hasListener = true;
-        }
-    };
+				documentElement.scrollTop = body.scrollTop = InOutCubic(elapsed, startPos, scrollEndValue, config.duration);
+				elapsed < config.duration ? requestAnimationFrame(scrollFrame) : args.callback();
+			};
+			requestAnimationFrame(scrollFrame);
+		} else {
+			documentElement.scrollTop = body.scrollTop = scrollEndValue;
+		}
+	};
 
-    return Object.freeze({
-        get: Object.freeze(config), // readonly
-        set: set, // also readonly, but setter
-        hookAnchors: hookAnchors,
-        scrollToElement: scrollToElement,
-        scroll: scroll,
-        scrollTop: scrollTop
-    });
+	// scrolls to specific Y axis location in relation to the root scroll location
+	var scrollTop = function scrollTop() {
+		var pos = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+		var args = arguments[1];
+		return scroll(pos - getScrollPosition(), args);
+	};
+
+	// scrolls to element on page (pass element as first argument, second argument optional)
+	var scrollToElement = function scrollToElement(elem, args) {
+		return (typeof elem === "undefined" ? "undefined" : _typeof(elem)) === "object" && elem !== null && scroll(elem.getBoundingClientRect().top, args);
+	};
+
+	var hookAnchors = function hookAnchors() {
+		if (!flags.hasListener) {
+			var listener = function listener(e) {
+				var target = e.target,
+				    hRef = target.getAttribute('href');
+
+				if (target.nodeName === 'A' && hRef.indexOf('#') < 2) {
+					// fix for pages with trailing '/'
+					hRef = hRef.indexOf('/#') === 0 ? hRef.substring(1) : hRef;
+
+					if (hRef.charAt(0) === '#') e.preventDefault();
+
+					if (hRef.length > 1) {
+						if (elem = document.getElementById(hRef.substring(1))) {
+							scrollToElement(elem, {
+								callback: function callback() {
+									config.updateURL && (window.location.href = hRef);
+								}
+							});
+						}
+					} else {
+						scrollToElement(body, {
+							callback: function callback() {
+								config.updateURL && (window.location.href = '#');
+							}
+						});
+					}
+				}
+			};
+
+			window.addEventListener('click', listener);
+			flags.hasListener = true;
+		}
+	};
+
+	return Object.freeze({
+		get: Object.freeze(config), // readonly
+		set: set, // also readonly, but setter
+		hookAnchors: hookAnchors,
+		scrollToElement: scrollToElement,
+		scroll: scroll,
+		scrollTop: scrollTop
+	});
 };
 /*__END_WRAP__*/
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	module.exports = jetpack;
+}
+
